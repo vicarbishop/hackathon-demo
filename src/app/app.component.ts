addfd7ebf86bf77fc8c1d6f522460589e0b15d65
TJ Miller <travis.miller@dotfoods.com>
2024-04-24 15:42:42 -0500

    env var test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hackathon';
+  isProduction = environment.production;
+
+  ngOnInit(): void {
+    console.log('Is production: ', this.isProduction);
+    console.log('Secret: ', environment.SECRET_GPT_API);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-      {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-      },
-      {
-        path: 'home',
-        loadComponent: () => import('./pages/home/home.component').then( m => m.HomeComponent)
-      },
-      {
-        path: 'ai',
-        loadComponent: () => import('./pages/ai/ai.component').then( m => m.AiComponent)
-      },
-      {
-        path: 'user',
-        loadComponent: () => import('./pages/user/user.component').then( m => m.UserComponent)
-      },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
 ];
