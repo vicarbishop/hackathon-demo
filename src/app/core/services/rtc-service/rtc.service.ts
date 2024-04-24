da7cbc8809e31831c26d20cb344c10d9cb83d7ee
Robyn Seale <robyn.seale@dotfoods.com>
2024-04-24 12:57:50 -0500

    add rtc and environement files

--- src/app/core/services/rtc-service/rtc.service.ts
+++ src/app/core/services/rtc-service/rtc.service.ts
@@ -1,18 +1,45 @@
 import { Injectable } from "@angular/core";
+import { app } from "../../../../../server";
 
 @Injectable({
     providedIn: "root",
   })
   
   export class RtcService {
-    getUsername(){}
-    createConnection(){}
-    endConnection(){}
-    updatePeer(){}
-    disconnectPeer(){}
-    configConnection(){}
+    appPrefix = "secure-p2p-multichat-";
+    username! : string;
+    peerError!: "";
+    loading: boolean = false;
+    peer: any = {};
+    targetIdInput: string = "";
+    peerIds!: string[];
+    connections: any = {};
+    chats!: any;
+    chatMessageInput!: string;
+
+    getUsername(user: string){
+      this.username = user;
+    }
+    createConnection(conn:any){
+      this.connections[conn.peer] = conn;
+      this.updatePeerIds();
+    }
+    endConnection(conn: any){      
+      this.connections.removeItem(conn.peer);
+      this.updatePeerIds();
+    }
+    updatePeerIds() {
+        this.peerIds = Object.keys(this.connections);
+    }
+    disconnectPeer(){
+      this.peer.disconnect();
+    }
+    configConnection(){
+      
+    }
     initConnection(){}
-    createPeer(){}
+    createPeer(){
+    }
     submitLogin(){}
     receiveChat(){}
     submitChat(){}
