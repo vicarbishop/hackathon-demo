d41042399ffae28daa698ca91a429cea69f58b58
AJ Jahjolli <aj.jahjolli@dotfoods.com>
2024-04-25 10:21:12 -0500

    Page UI Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../../environments/environments';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-root',
